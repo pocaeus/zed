8fbcaff4ddde0626128b68f33a596096ddbd7b07
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2023-09-20 21:49:06 +0200

    Another wip

--- crates/zed/src/main.rs
+++ crates/zed/src/main.rs
@@ -35,6 +35,7 @@ use std::{
     os::unix::prelude::OsStrExt,
     panic,
     path::{Path, PathBuf},
+    process::{ChildStdout, Stdio},
     str,
     sync::{
         atomic::{AtomicBool, AtomicU32, Ordering},
@@ -60,72 +61,8 @@ use zed::{
     build_window_options, handle_keymap_file_changes, initialize_workspace, languages, menus,
     only_instance::{ensure_only_instance, IsOnlyInstance},
 };
-const SOCKET_NAME: &str = "minidumper-disk-example";
+
 fn main() {
-    if std::env::args().any(|a| a == "--server") {
-        let mut server =
-            minidumper::Server::with_name(SOCKET_NAME).expect("failed to create server");
-
-        let ab = std::sync::atomic::AtomicBool::new(false);
-
-        struct Handler;
-
-        impl minidumper::ServerHandler for Handler {
-            /// Called when a crash has been received and a backing file needs to be
-            /// created to store it.
-            fn create_minidump_file(
-                &self,
-            ) -> Result<(std::fs::File, std::path::PathBuf), std::io::Error> {
-                let uuid = uuid::Uuid::new_v4();
-
-                let dump_path = std::path::PathBuf::from(format!("dumps/{uuid}.dmp"));
-                println!("{:?}", &dump_path);
-                if let Some(dir) = dump_path.parent() {
-                    if !dir.try_exists()? {
-                        std::fs::create_dir_all(dir)?;
-                    }
-                }
-                let file = std::fs::File::create(&dump_path)?;
-                Ok((file, dump_path))
-            }
-
-            /// Called when a crash has been fully written as a minidump to the provided
-            /// file. Also returns the full heap buffer as well.
-            fn on_minidump_created(
-                &self,
-                result: Result<minidumper::MinidumpBinary, minidumper::Error>,
-            ) -> minidumper::LoopAction {
-                match result {
-                    Ok(mut md_bin) => {
-                        use std::io::Write;
-                        let _ = md_bin.file.flush();
-                        println!("wrote minidump to disk");
-                    }
-                    Err(e) => {
-                        log::error!("failed to write minidump: {:#}", e);
-                    }
-                }
-
-                // Tells the server to exit, which will in turn exit the process
-                minidumper::LoopAction::Exit
-            }
-
-            fn on_message(&self, kind: u32, buffer: Vec<u8>) {
-                log::info!(
-                    "kind: {kind}, message: {}",
-                    String::from_utf8(buffer).unwrap()
-                );
-            }
-        }
-
-        server
-            .run(Box::new(Handler), &ab, None)
-            .expect("failed to run server");
-
-        return;
-    }
-    // Attempt to connect to the server
-
     let http = http::client();
     init_paths();
     init_logger();
@@ -477,41 +414,7 @@ static PANIC_COUNT: AtomicU32 = AtomicU32::new(0);
 fn init_panic_hook(app: &App, installation_id: Option<String>) {
     let is_pty = stdout_is_a_pty();
     let platform = app.platform();
-    let mut server = None;
-    let (client, _server) = loop {
-        if let Ok(client) = minidumper::Client::with_name(SOCKET_NAME) {
-            break (client, server.unwrap());
-        }
 
-        let exe = std::env::current_exe().expect("unable to find ourselves");
-
-        server = Some(
-            std::process::Command::new(exe)
-                .arg("--server")
-                .spawn()
-                .expect("unable to spawn server process"),
-        );
-
-        // Give it time to start
-        std::thread::sleep(std::time::Duration::from_millis(100));
-    };
-    println!("Connected");
-    let handler = crash_handler::CrashHandler::attach(unsafe {
-        crash_handler::make_crash_event(move |crash_context: &crash_handler::CrashContext| {
-            // Before we request the crash, send a message to the server
-            client.send_message(2, "mistakes were made").unwrap();
-
-            // Send a ping to the server, this ensures that all messages that have been sent
-            // are "flushed" before the crash event is sent. This is only really useful
-            // on macos where messages and crash events are sent via different, unsynchronized,
-            // methods which can result in the crash event closing the server before
-            // the non-crash messages are received/processed
-            client.ping().unwrap();
-
-            crash_handler::CrashEventResult::Handled(client.request_dump(crash_context).is_ok())
-        })
-    })
-    .expect("failed to attach signal handler");
     panic::set_hook(Box::new(move |info| {
         let prior_panic_count = PANIC_COUNT.fetch_add(1, Ordering::SeqCst);
         if prior_panic_count > 0 {
@@ -521,9 +424,10 @@ fn init_panic_hook(app: &App, installation_id: Option<String>) {
             }
         }
 
+        // client.request_dump(crash_context);
+
         let thread = thread::current();
         let thread_name = thread.name().unwrap_or("<unnamed>");
-        handler.simulate_exception(None);
         let payload = info
             .payload()
             .downcast_ref::<&str>()
@@ -534,15 +438,21 @@ fn init_panic_hook(app: &App, installation_id: Option<String>) {
         if *util::channel::RELEASE_CHANNEL == ReleaseChannel::Dev {
             let location = info.location().unwrap();
             let backtrace = Backtrace::new();
-            eprintln!(
-                "Thread {:?} panicked with {:?} at {}:{}:{}\n{:?}",
-                thread_name,
-                payload,
-                location.file(),
-                location.line(),
-                location.column(),
-                backtrace,
-            );
+            // eprintln!(
+            //     "Thread {:?} panicked with {:?} at {}:{}:{}\n{:?}",
+            //     thread_name,
+            //     payload,
+            //     location.file(),
+            //     location.line(),
+            //     location.column(),
+            //     backtrace,
+            // );
+            let mut writer = minidump_writer::minidump_writer::MinidumpWriter::new(None, None);
+            let mut minidump_file =
+                std::fs::File::create("example_dump.mdmp").expect("failed to create file");
+            writer
+                .dump(&mut minidump_file)
+                .expect("failed to write minidump");
             std::process::exit(-1);
         }
 
